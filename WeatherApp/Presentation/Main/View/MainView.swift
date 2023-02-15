c3e1f8dd7cfedf16e4594982009177bb21297b24
Safari <111dltldnjs@gmail.com>
2023-02-15 20:00:00 +0900

    feat: MainView ScrollView layout 적용

--- WeatherApp/Presentation/Main/View/MainView.swift
+++ WeatherApp/Presentation/Main/View/MainView.swift
@@ -10,4 +10,27 @@ import UIKit
 import SnapKit
 
 final class MainView: UIView {
+  private let scrollView = UIScrollView()
+  
+  init() {
+    super.init(frame: .zero)
+    attribute()
+    layout()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func attribute() {
+    scrollView.showsVerticalScrollIndicator = false
+  }
+  
+  private func layout() {
+    addSubview(scrollView)
+    
+    scrollView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+  }
 }
