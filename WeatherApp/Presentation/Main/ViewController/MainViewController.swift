a33d167fa0513b1082d9ee2bd455a07735fc2c74
Safari <111dltldnjs@gmail.com>
2023-02-15 19:37:24 +0900

    chore: MainVC final 키워드 추가

--- WeatherApp/Presentation/Main/ViewController/MainViewController.swift
+++ WeatherApp/Presentation/Main/ViewController/MainViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+final class MainViewController: UIViewController {
   weak var coordinator: MainCoordinator?
   private let searchBar = UISearchBar()
   
