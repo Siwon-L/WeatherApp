7035e4c0ebfe367c00fe969d56c7d2487fb0a906
Safari <111dltldnjs@gmail.com>
2023-02-16 14:20:26 +0900

    feat: api 키를 가져올 Bundle Extension

---
+++ WeatherApp/Utils/Bundle+Extension.swift
@@ -0,0 +1,29 @@
+//
+//  Bundle+Extension.swift
+//  WeatherApp
+//
+//  Created by 이시원 on 2023/02/16.
+//
+
+import Foundation
+
+extension Bundle {
+  var apiKey: String {
+    guard let file = self.path(forResource: "WeatherAPIInfo", ofType: "plist") else {
+      return ""
+    }
+    
+    guard let url = URL(string: file) else {
+      return ""
+    }
+    
+    guard let resource = try? NSDictionary(contentsOf: url, error: ()) else {
+      return ""
+    }
+    
+    guard let key = resource["API_KEY"] as? String else {
+      return ""
+    }
+    return ""
+  }
+}
