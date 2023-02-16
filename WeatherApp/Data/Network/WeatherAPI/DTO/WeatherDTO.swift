57bb8caab2fd8d7edc4f13a1efe21faa1c03cc49
Safari <111dltldnjs@gmail.com>
2023-02-16 17:04:36 +0900

    feat: WeatherDTO 구현

--- WeatherApp/Data/Network/WeatherAPI/DTO/WeatherDTO.swift
+++ WeatherApp/Data/Network/WeatherAPI/DTO/WeatherDTO.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct WeatherDTO: Codable {
+  let id: Int
+  let weatherCase: WeatherCaseDTO
+  let description: String
+  let icon: String
+  
+  private enum CodingKeys: String, CodingKey {
+    case id
+    case weatherCase = "main"
+    case description, icon
+  }
+}
