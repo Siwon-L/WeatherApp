45106a669c1d5991b9b1682d9f333ab58c865b41
Safari <111dltldnjs@gmail.com>
2023-02-16 17:17:37 +0900

    feat: ObservationInfoDTO 구현

--- WeatherApp/Data/Network/WeatherAPI/DTO/ObservationInfoDTO.swift
+++ WeatherApp/Data/Network/WeatherAPI/DTO/ObservationInfoDTO.swift
@@ -7,4 +7,24 @@
 
 import Foundation
 
+struct ObservationInfoDTO: Codable {
+  let time: Int
+  let climate: ClimateDTO
+  let weather: [WeatherDTO]
+  let clouds: CloudsDTO
+  let wind: WindDTO
+  let visibility: Int
+  let pop: Double
+  let sys: SysDTO
+  let dtTxt: String
+  let rain: RainDTO?
+  let snow: RainDTO?
+  
+  enum CodingKeys: String, CodingKey {
+    case time = "dt"
+    case climate = "main"
+    case weather, clouds, wind, visibility, pop, sys
+    case dtTxt = "dt_txt"
+    case rain, snow
+  }
 }
