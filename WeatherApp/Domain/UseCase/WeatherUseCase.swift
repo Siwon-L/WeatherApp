01c78b94d83d62c24dceb4d7634afbe1c6c149c5
Safari <111dltldnjs@gmail.com>
2023-02-16 21:38:02 +0900

    feat: WeatherUseCase 구현

--- WeatherApp/Domain/UseCase/WeatherUseCase.swift
+++ WeatherApp/Domain/UseCase/WeatherUseCase.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+import RxSwift
+
+protocol WeatherUseCaseable {
+  func requestWeather(lat: Double, lon: Double) -> Observable<WeatherInfo>
+}
+
+final class WeatherUseCase: WeatherUseCaseable {
+  private let repository: WeatherRepositoryable
+  
+  init(repository: WeatherRepositoryable) {
+    self.repository = repository
+  }
+  
+  func requestWeather(lat: Double, lon: Double) -> Observable<WeatherInfo> {
+    return repository.requestWeather(lat: lat, lon: lon)
+  }
+}
