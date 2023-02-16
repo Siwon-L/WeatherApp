043aa95a7e53a4b34cc986b960cf0a6e7005530a
Safari <111dltldnjs@gmail.com>
2023-02-16 14:20:59 +0900

    feat: WeatherEndpointAPI 구현

--- WeatherApp/Data/Network/WeatherAPI/WeatherEndpointAPI.swift
+++ WeatherApp/Data/Network/WeatherAPI/WeatherEndpointAPI.swift
@@ -8,5 +8,29 @@
 import Foundation
 
 enum WeatherEndpointAPI {
+  private var baseURL: String {
+    "https://api.openweathermap.org"
+  }
   
+  case weather(
+    lat: Double,
+    lon: Double
+  )
+  
+  var asEndpoint: Endpoint {
+    switch self {
+    case .weather(let lat, let lon):
+      return Endpoint(
+        base: baseURL,
+        path: "/data/2.5/forecast",
+        method: .get,
+        queries: [
+          "lat": lat,
+          "lon": lon,
+          "appid": Bundle.main.apiKey,
+          "units": "metric"
+        ]
+      )
+    }
+  }
 }
