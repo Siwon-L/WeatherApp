d44171dd0787863d309912c4333c5637a80d4041
Safari <111dltldnjs@gmail.com>
2023-02-16 22:47:59 +0900

    feat: DayWeatherInfo 구현

---
+++ WeatherApp/Domain/Entity/DayWeatherInfo.swift
@@ -0,0 +1,15 @@
+//
+//  DayWeatherInfo.swift
+//  WeatherApp
+//
+//  Created by 이시원 on 2023/02/16.
+//
+
+import Foundation
+
+struct DayWeatherInfo {
+  let day: String
+  let weatherCase: WeatherCase
+  let teamMax: Double
+  let teamMin: Double
+}
