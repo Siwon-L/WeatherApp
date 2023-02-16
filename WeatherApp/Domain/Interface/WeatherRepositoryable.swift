01527c680232f85d1f3f40e9b4cd20000ae7556a
Safari <111dltldnjs@gmail.com>
2023-02-16 21:29:37 +0900

    feat: WeatherRepositoryable protocol 구현

---
+++ WeatherApp/Domain/Interface/WeatherRepositoryable.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherRepositoryable.swift
+//  WeatherApp
+//
+//  Created by 이시원 on 2023/02/16.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol WeatherRepositoryable {
+  func requestWeather(lat: Double, lon: Double) -> Observable<WeatherInfo>
+}
