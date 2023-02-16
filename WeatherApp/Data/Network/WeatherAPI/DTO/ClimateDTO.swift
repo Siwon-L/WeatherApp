77b88887f51b7a8cc880b862924d2e32020ee682
Safari <111dltldnjs@gmail.com>
2023-02-16 16:53:41 +0900

    feat: ClimateDTO 구현

--- WeatherApp/Data/Network/WeatherAPI/DTO/ClimateDTO.swift
+++ WeatherApp/Data/Network/WeatherAPI/DTO/ClimateDTO.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct ClimateDTO: Codable {
+  let temp: Double
+  let feelsLike: Double
+  let tempMin: Double
+  let tempMax: Double
+  let pressure: Int
+  let seaLevel: Int
+  let grndLevel: Int
+  let humidity: Int
+  let tempKf: Double
+  
+  enum CodingKeys: String, CodingKey {
+    case temp
+    case feelsLike = "feels_like"
+    case tempMin = "temp_min"
+    case tempMax = "temp_max"
+    case pressure
+    case seaLevel = "sea_level"
+    case grndLevel = "grnd_level"
+    case humidity
+    case tempKf = "temp_kf"
+  }
+}
