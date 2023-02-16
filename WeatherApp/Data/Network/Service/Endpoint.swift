e7954cab0d67a4641f902a1318d9f3cc9e032b89
Safari <111dltldnjs@gmail.com>
2023-02-16 13:51:15 +0900

    feat: Endpoint 구현

--- WeatherApp/Data/Network/Service/Endpoint.swift
+++ WeatherApp/Data/Network/Service/Endpoint.swift
@@ -8,5 +8,38 @@
 import Foundation
 
 final class Endpoint {
+  enum HTTPMethod: String {
+    case get = "GET"
+  }
   
+  private let base: String
+  private let path: String
+  private let method: HTTPMethod
+  private let queries: [String: Any]
+  
+  init(
+    base: String,
+    path: String,
+    method: HTTPMethod,
+    queries: [String : Any] = [:]
+  ) {
+    self.base = base
+    self.path = path
+    self.method = method
+    self.queries = queries
+  }
+  
+  private func createURL() throws -> URL {
+    let urlString = base + path
+    
+    var component = URLComponents(string: urlString)
+    component?.queryItems = queries.map {
+      URLQueryItem(name: $0.key, value: "\($0.value)")
+    }
+    
+    guard let url = component?.url else {
+      throw HTTPError.createURLError
+    }
+    return url
+  }
 }
