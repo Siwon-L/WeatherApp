e94817508c40737b76dbb93c366322198f188dd3
Safari <111dltldnjs@gmail.com>
2023-02-18 06:15:10 +0900

    feat: SearchViewController bind 메서드

--- WeatherApp/Presentation/Search/ViewController/SearchViewController.swift
+++ WeatherApp/Presentation/Search/ViewController/SearchViewController.swift
@@ -7,10 +7,15 @@
 
 import UIKit
 
+import RxSwift
+
 final class SearchViewController: UIViewController {
   private let tableView = UITableView()
+  private let viewModel: SearchViewModelable
+  private let disposeBag = DisposeBag()
   
-  init() {
+  init(viewModel: SearchViewModelable) {
+    self.viewModel = viewModel
     super.init(nibName: nil, bundle: nil)
   }
   
@@ -22,14 +27,56 @@ final class SearchViewController: UIViewController {
     super.viewDidLoad()
     attribute()
     layout()
+    bind(viewModel)
   }
   
   private func attribute() {
     view.backgroundColor = .systemIndigo
     
+    tableView.then {
+      $0.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.identifier)
+      $0.backgroundColor = .clear
+    }
   }
   
   private func layout() {
-
+    view.addSubview(tableView)
+    
+    tableView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+  }
+  
+  private func bind(_ viewModel: SearchViewModelable) {
+    bindInput(viewModel)
+    bindOutput(viewModel)
+  }
+  
+  private func bindInput(_ viewModel: SearchViewModelable) {
+    viewModel.parsingCityList()
+    
+    tableView.rx.itemSelected
+      .bind(with: self) { owner, indexPath in
+        owner.viewModel.selectedCity(indexPath: indexPath)
+        owner.dismiss(animated: true)
+      }.disposed(by: disposeBag)
+    
+    NotificationCenter.default.rx.notification(.searchKetWord)
+      .compactMap { $0.object as? String }
+      .bind(with: self) { owner, keyWord in
+        owner.viewModel.searchKeyWord(keyWord)
+      }.disposed(by: disposeBag)
+  }
+  
+  private func bindOutput(_ viewModel: SearchViewModelable) {
+    viewModel.searchedList
+      .bind(
+        to: tableView.rx.items(
+          cellIdentifier: SearchTableViewCell.identifier,
+          cellType: SearchTableViewCell.self
+        )
+      ) { _, city, cell in
+        cell.bindView(city: city)
+      }.disposed(by: disposeBag)
   }
 }
