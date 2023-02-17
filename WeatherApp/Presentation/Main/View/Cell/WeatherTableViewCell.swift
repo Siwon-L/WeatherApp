abb524db9b00f6b3189d4263b4325a7ebf0c0059
Safari <111dltldnjs@gmail.com>
2023-02-17 22:57:45 +0900

    refactor: WeatherTableViewCell layout 수정

--- WeatherApp/Presentation/Main/View/Cell/WeatherTableViewCell.swift
+++ WeatherApp/Presentation/Main/View/Cell/WeatherTableViewCell.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 import SnapKit
+import RxSwift
 
 final class WeatherTableViewCell: UITableViewCell {
   static let identifier = "WeatherTableViewCell"
-  let dayLabel = UILabel()
-  let weatherIconImageView = UIImageView()
-  let minTempLabel = UILabel()
-  let maxTempLabel = UILabel()
-  private let mainStackView = UIStackView()
+  private let dayLabel = UILabel()
+  private let weatherIconImageView = UIImageView()
+  private let minTempLabel = UILabel()
+  private let maxTempLabel = UILabel()
   private let tempStackView = UIStackView()
   
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -29,32 +29,42 @@ final class WeatherTableViewCell: UITableViewCell {
   }
   
   private func attribute() {
-    dayLabel.then {
-      $0.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-    }
+    backgroundColor = .clear
     
     tempStackView.then {
       $0.addArrangedSubviews([
         minTempLabel,
         maxTempLabel
       ])
-      $0.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-    }
-    
-    mainStackView.then {
-      $0.addArrangedSubviews([
-        dayLabel,
-        weatherIconImageView,
-        tempStackView
-      ])
+      $0.spacing = 8
     }
   }
   
   private func layout() {
-    addSubview(mainStackView)
+    addSubviews([
+      dayLabel,
+      weatherIconImageView,
+      tempStackView
+    ])
     
-    mainStackView.snp.makeConstraints {
-      $0.directionalEdges.equalToSuperview()
+    dayLabel.snp.makeConstraints {
+      $0.leading.equalToSuperview().inset(20)
+      $0.centerY.equalToSuperview()
+    }
+    
+    tempStackView.snp.makeConstraints {
+      $0.trailing.equalToSuperview().inset(20)
+      $0.centerY.equalToSuperview()
+    }
+    
+    weatherIconImageView.snp.makeConstraints {
+      $0.centerY.equalToSuperview()
+      $0.centerX.equalToSuperview().multipliedBy(0.7)
+      $0.height.width.equalTo(30)
+    }
+    
+    weatherIconImageView.snp.makeConstraints {
+      $0.width.equalTo(weatherIconImageView.snp.height)
     }
   }
 }
