1e59d5253e93a821d030328495b5766c53dd1580
Safari <111dltldnjs@gmail.com>
2023-02-18 07:41:04 +0900

    feat: RoundMapView 구현

--- WeatherApp/Presentation/Main/View/RoundMapView.swift
+++ WeatherApp/Presentation/Main/View/RoundMapView.swift
@@ -5,4 +5,39 @@
 //  Created by 이시원 on 2023/02/18.
 //
 
-import Foundation
+import UIKit
+import MapKit
+
+import SnapKit
+
+final class RoundMapView: UIView {
+  let mapView = MKMapView()
+  private(set) lazy var roundView = RoundView(view: mapView)
+  
+  init() {
+    super.init(frame: .zero)
+    attribute()
+    layout()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func attribute() {
+    mapView.then {
+      $0.isScrollEnabled = false
+      $0.isRotateEnabled = false
+      $0.isPitchEnabled = false
+      $0.isZoomEnabled = false
+    }
+  }
+  
+  private func layout() {
+    addSubview(roundView)
+    
+    roundView.snp.makeConstraints {
+      $0.directionalEdges.equalToSuperview()
+    }
+  }
+}
