07918bf63c0dea58276d69c1b14cecda32cce15a
Safari <111dltldnjs@gmail.com>
2023-02-17 17:10:30 +0900

    feat: Table, Collection Cell 구현

--- WeatherApp/Presentation/Main/View/Cell/WeatherCollectionViewCell.swift
+++ WeatherApp/Presentation/Main/View/Cell/WeatherCollectionViewCell.swift
@@ -5,4 +5,44 @@
 //  Created by 이시원 on 2023/02/17.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+final class WeatherCollectionViewCell: UICollectionViewCell {
+  static let identifier = "WeatherCollectionViewCell"
+  let timeLabel = UILabel()
+  let weatherIconImageView = UIImageView()
+  let tempLabel = UILabel()
+  private let mainStackView = UIStackView()
+
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    attribute()
+    layout()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func attribute() {
+    mainStackView.then {
+      $0.axis = .vertical
+      $0.addArrangedSubviews([
+        timeLabel,
+        weatherIconImageView,
+        tempLabel
+      ])
+    }
+  }
+  
+  private func layout() {
+    addSubview(mainStackView)
+    
+    mainStackView.snp.makeConstraints {
+      $0.directionalEdges.equalToSuperview()
+    }
+  }
+}
--- WeatherApp/Presentation/Main/View/Cell/WeatherTableViewCell.swift
+++ WeatherApp/Presentation/Main/View/Cell/WeatherTableViewCell.swift
@@ -5,4 +5,56 @@
 //  Created by 이시원 on 2023/02/17.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+final class WeatherTableViewCell: UITableViewCell {
+  static let identifier = "WeatherTableViewCell"
+  let dayLabel = UILabel()
+  let weatherIconImageView = UIImageView()
+  let minTempLabel = UILabel()
+  let maxTempLabel = UILabel()
+  private let mainStackView = UIStackView()
+  private let tempStackView = UIStackView()
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    attribute()
+    layout()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func attribute() {
+    dayLabel.then {
+      $0.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+    }
+    
+    tempStackView.then {
+      $0.addArrangedSubviews([
+        minTempLabel,
+        maxTempLabel
+      ])
+      $0.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+    }
+    
+    mainStackView.then {
+      $0.addArrangedSubviews([
+        dayLabel,
+        weatherIconImageView,
+        tempStackView
+      ])
+    }
+  }
+  
+  private func layout() {
+    addSubview(mainStackView)
+    
+    mainStackView.snp.makeConstraints {
+      $0.directionalEdges.equalToSuperview()
+    }
+  }
+}
