0c195e3499a60b6adfc55690fd8bcfe5996e7b78
Safari <111dltldnjs@gmail.com>
2023-02-18 05:08:59 +0900

    chore: NSNotification Name 정의

---
+++ WeatherApp/Presentation/Extension/NSNotificationName+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  NSNotificationName+Extension.swift
+//  WeatherApp
+//
+//  Created by 이시원 on 2023/02/18.
+//
+
+import Foundation
+
+extension NSNotification.Name {
+  static let searchKetWord = NSNotification.Name("SearchKetWord")
+  static let selectedCity = NSNotification.Name("SelectedCity")
+}
